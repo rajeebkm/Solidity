8d0b1ebb96282c327424ed4e2aa4bd6954c88525
rajeebkm <rkm2541997@gmail.com>
2022-03-14 02:46:22 +0530

    Solidity Smart Contracts Added

--- Smart Contracts/governance/IGovernor.sol
+++ Smart Contracts/governance/IGovernor.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v4.5.0) (governance/IGovernor.sol)
 
 pragma solidity ^0.8.0;
 
--- Smart Contracts/governance/utils/Votes.sol
+++ Smart Contracts/governance/utils/Votes.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v4.5.0) (governance/utils/Votes.sol)
 pragma solidity ^0.8.0;
 
 import "../../utils/Context.sol";
