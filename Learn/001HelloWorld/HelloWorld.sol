323045a51673f8f5afe5fd40419b5d30ac287320
rajeebkm <rkm2541997@gmail.com>
2022-06-08 18:10:28 +0530

    Hello world added

---
+++ Learn/001HelloWorld/HelloWorld.sol
@@ -0,0 +1,11 @@
+// Hello World
+
+// pragma specifies the compiler version of Solidity.
+
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.13 and less than 0.9.0
+pragma solidity ^0.8.13;
+
+contract HelloWorld {
+    string public greet = "Hello World!";
+}
