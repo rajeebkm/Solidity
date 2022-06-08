6eceb26b3cc916478819276e094d71723ccccca9
rajeebkm <rkm2541997@gmail.com>
2022-06-08 18:55:43 +0530

    Read and write to a state variable updated

--- Learn/007ReadAndWriteStateVariable/ReadAndWriteStateVariable.sol
+++ Learn/007ReadAndWriteStateVariable/ReadAndWriteStateVariable.sol
@@ -4,18 +4,21 @@
 
 // On the other hand, you can read state variables, for free, without any transaction fee.
 
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
-contract SimpleStorage{
-
+contract SimpleStorage {
+    // State variable to store a number
     uint public num;
 
+    // You need to send a transaction to write to a state variable.
     function set(uint _num) public {
         num = _num;
     }
 
-    function get() public view returns(uint){
+    // You can read from a state variable without sending a transaction.
+    function get() public view returns (uint) {
         return num;
-
     }
 }
+
