e7849b5d316a191925f02ae8b3f4c5352928a8a5
rajeebkm <rkm2541997@gmail.com>
2022-06-30 12:29:34 +0530

    Structs added

---
+++ Learn/015Structs/StructDeclaration.sol
@@ -0,0 +1,16 @@
+// Structs
+
+// You can define your own type by creating a struct.
+
+// They are useful for grouping together related data.
+
+// Structs can be declared outside of a contract and imported in another contract.
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+// This is saved 'StructDeclaration.sol'
+
+struct Todo {
+    string text;
+    bool completed;
+}
